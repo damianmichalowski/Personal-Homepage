e66c3c69158dc932525aababc77c7e57f15b8578
Damian Michalowski <dowski888@gmail.com>
2022-06-15 14:48:26 +0200

    initial commit

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    color: {
+        teal: "#008080",
+        white: "#FFFFFF",
+        lightGrey: "#B3B3B3",
+        bilbao: "#1A7613",
+        red: "#c91818",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
